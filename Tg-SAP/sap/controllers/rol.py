9e78a1a4fceb7915aecd39181aafb27c18780cc5
rbanuelos <robertobanuelos@gmail.com>
2011-04-24 11:32:12 -0400

    se agrego el controlador de roles

--- Tg-SAP/sap/controllers/rol.py
+++ Tg-SAP/sap/controllers/rol.py
@@ -8,73 +8,66 @@ from sap.model import DBSession, metadata
 
 
 """
-Import widget
+Import widgets
 """
 from tg import tmpl_context
 from sap.widgets.createform import *
+from sap.widgets.listform import *
+from sap.widgets.editform import *
 
 from sap.model import *
 from tg import tmpl_context, redirect, validate
 
-class RolController(BaseController):
-	"""
-	prueba de abm y de visibilidad de datos en el template master.html
-	anhadi py:if="tg.predicates.has_permission('manage')" para que solo
-	muestre el link a los usuarios que posean el permiso 'manage'
-	ademas los metodos de new_usuario, create_usuario y list_usuario estan
-	anotados con @require(predicates.has_permission('manage')) esto es para que 
-	no se pueda acceder a al formulario atravez de la url.
-	"""
+from tg.controllers import RestController
+
+class RolController(RestController):
+
 	@expose('sap.templates.new')
 	@require(predicates.has_permission('manage'))
-	def new (self, **kw):
-		"""
-		Despliega en pantalla el widget ProyectoForm que se encuentra
-		en la carpeta widget
-		"""
-		tmpl_context.form = create_rol_form
+	def new(self, **kw):
+		tmpl_context.widget = new_rol_form
+		return dict(value=kw, modelname='Rol')
 		
-		return dict(modelname='Rol',
-			genre_options=DBSession.query(Rol.id_rol),
-			page='Nuevo Rol')
-
-
-	@validate(create_rol_form, error_handler=new)
-	@expose('sap.templates.new')
-	@require(predicates.has_permission('manage'))
-	def create(self, **kw):
-		"""
-		El formulario envia los argumentos del formulario 
-		a este metodo en la variable kw y estos seteados a una variable
-		del tipo Proyecto.
-		"""
-		rol = Rol()
-		rol.nombre = kw['nombre']
-		rol.descripcion = kw['descripcion']
-		#guarda el usuario registrado en el formulario
+	@validate(new_rol_form, error_handler=new)
+	@expose()
+	def post(self, modelname, **kw):
+		del kw['sprox_id']
+		rol = Rol(**kw)
 		DBSession.add(rol)
-		#emite un mensaje y redirecciona a la pagina de listado
-		flash("El Rol ha sido creado correctamente.")
-		redirect("list")
-		
+		flash("El rol ha sido creado correctamente.")
+		redirect("/rol/list")
+	
+	@expose('sap.templates.edit')
+	@require(predicates.has_permission('manage'))
+	def edit(self, id,**kw):
+		rol =  DBSession.query(Rol).get(id)
+		tmpl_context.widget = rol_edit_form
+		kw['id_rol'] = rol.id_rol
+		kw['nombre'] = rol.nombre
+		kw['descripcion'] = rol.descripcion
+		return dict(value=kw, modelname='Rol')
+	
+	@validate(rol_edit_form, error_handler=edit)
+	@expose()
+	def put(self, _method, **kw):
+		del kw['sprox_id']
+		rol = Rol(**kw)
+		DBSession.merge(rol)
+		flash("El rol ha sido modificado correctamente.")
+		redirect("/rol/list")
+	
 
 	@expose('sap.templates.list')
 	@require(predicates.has_permission('manage'))
 	def list(self, **kw):
 		"""Lista todos los roles de la base de datos"""
-		list = DBSession.query(Rol)
-		items = [] 
-		head = []
-		'''
-		Se anhade el nombre de las columnas
-		'''
-		head.append(['ID', 'Nombre', 'Descripcion'] )
-					
-		'''
-		Se formatea la entidad como una matriz para ser visualizada en la tabla
-		'''
-		for item in list:
-			items.append([item.id_rol, item.nombre, item.descripcion])
-		
-		return dict(array=items, headers=head, modelname='rol',
-					page='Lista de Roles')
+		tmpl_context.widget = rol_table
+		value = rol_filter.get_value()
+		return dict(modelname='Rol',value=value)
+	
+	@expose()
+	def post_delete(self, id_rol, **kw):
+		DBSession.delete(DBSession.query(Rol).get(id_rol))
+		flash("El rol ha sido "+ id_rol +" eliminado correctamente.")
+		redirect("/rol/list")
+	
